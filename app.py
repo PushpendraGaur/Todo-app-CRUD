bd5f875a4ed892936dc4f02f0fd03abb981892b6
Pushpendra Gaur <push40662@gmail.com>
2021-06-22 16:10:14 +0530

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 from werkzeug.utils import redirect
 from pymongo import MongoClient
-from datetime import date, datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
